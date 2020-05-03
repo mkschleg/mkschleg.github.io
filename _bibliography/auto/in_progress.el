18a962a022237ad3655191e720ae9a2a3f12acd9
Matthew Schlegel <mkschleg@gmail.com>
2020-05-03 17:38:06 -0600

    Remove old website

--- _bibliography/auto/in_progress.el
+++
@@ -1,7 +0,0 @@
-(TeX-add-style-hook
- "in_progress"
- (lambda ()
-   (LaTeX-add-bibitems
-    "schlegel2018general"))
- :bibtex)
-
--- _bibliography/auto/workshop_pubs.el
+++
@@ -1,8 +0,0 @@
-(TeX-add-style-hook
- "workshop_pubs"
- (lambda ()
-   (LaTeX-add-bibitems
-    "schlegel2018baseline"
-    "schlegel2017stable"))
- :bibtex)
-
